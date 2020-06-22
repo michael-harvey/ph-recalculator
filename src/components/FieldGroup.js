c1b4c90ab8b3d2440d7a00451c3765cbeae5bf91
Mike Harvey <harvey.miche@gmail.com>
2020-06-22 22:50:51 +1200

    Remove table due to responsive issues

--- src/components/FieldGroup.js
+++ src/components/FieldGroup.js
@@ -84,34 +84,24 @@ function FieldGroup(props) {
         />
       </form>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Ionic strength:</th>
-            <th>SO4 total:</th>
-            <th>Potassium bisulfate:</th>
-            <th>Total pH:</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{hasAllValues ? calculateIonicStrength() : "-"}</td>
-            <td>{hasAllValues ? calculateSO4Total() : "-"}</td>
-            <td>{hasAllValues ? calculatePotassiumBisulfate() : "-"}</td>
-            <td>
-              {hasAllValues ? calculateTotalPh() : "-"}
-              {/* <svg
-              width="20"
-              fill="#fff"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 100 125"
-            >
-              <path d="M36 6c-3.29 0-6 2.71-6 6v10h-6c-3.29 0-6 2.71-6 6v60c0 3.29 2.71 6 6 6h40c3.29 0 6-2.71 6-6V78h6c3.29 0 6-2.71 6-6V26a2 2 0 00-.594-1.406l-18-18A2 2 0 0062 6H44a2 2 0 100 4h16v16a2 2 0 002 2h16v44c0 1.143-.857 2-2 2H36c-1.143 0-2-.857-2-2V12c0-1.143.857-2 2-2a2 2 0 100-4zm28 6.813L75.188 24H64V12.812zM24 26h6v46c0 3.29 2.71 6 6 6h30v10c0 1.143-.857 2-2 2H24c-1.143 0-2-.857-2-2V28c0-1.143.857-2 2-2z" />
-            </svg> */}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <ul>
+        <li>
+          <b>Ionic strength:</b>
+          {hasAllValues ? calculateIonicStrength() : "-"}
+        </li>
+        <li>
+          <b>SO4 total:</b>
+          {hasAllValues ? calculateSO4Total() : "-"}
+        </li>
+        <li>
+          <b>Potassium bisulfate:</b>
+          {hasAllValues ? calculatePotassiumBisulfate() : "-"}
+        </li>
+        <li>
+          <b>Total pH:</b>
+          {hasAllValues ? calculateTotalPh() : "-"}
+        </li>
+      </ul>
     </div>
   );
 }
