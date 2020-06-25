acb9acedc5048362ea5dc09ee552e1962e04ad34
Mike Harvey <harvey.miche@gmail.com>
2020-06-26 00:15:57 +1200

    Remove unused import

--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders app", () => {
