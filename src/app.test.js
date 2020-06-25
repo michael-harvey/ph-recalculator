1de6c1c5a248b11c03f3c31aa45616bc0a20ff32
Mike Harvey <harvey.miche@gmail.com>
2020-06-26 00:14:54 +1200

    Add test for expected data results

--- src/app.test.js
+++ src/app.test.js
@@ -8,12 +8,29 @@ test("renders app", () => {
   expect(getByText("pH Recalculator")).toBeInTheDocument();
 });
 
-test("adds additional fields", () => {
-  render(<App />);
+test("displays expected total ph from values", () => {
+  const { getByLabelText, queryByTestId } = render(<App />);
 
-  fireEvent.click(screen.getByText("Add field group"));
-  expect();
-  // TODO: test for two sets of fields
+  fireEvent.change(getByLabelText(/ph/i), {
+    target: { value: "8.1" },
+  });
+
+  fireEvent.change(getByLabelText(/salinity/i), {
+    target: { value: "35" },
+  });
+
+  fireEvent.change(getByLabelText(/temperature/i), {
+    target: { value: "18" },
+  });
+
+  expect(queryByTestId("ionic-strength")).toHaveTextContent(
+    "0.7227631953981292"
+  );
+  expect(queryByTestId("so4-total")).toHaveTextContent("0.028235434132860126");
+  expect(queryByTestId("potassium-bisulfate")).toHaveTextContent(
+    "0.027377471695080157"
+  );
+  expect(queryByTestId("total-ph")).toHaveTextContent("7.975298869024447");
 });
 
 // TODO: test removal of fields
